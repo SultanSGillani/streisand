f73ca6a69d006ad43f601310dae110c4642dfb15
saltman <saltman@jumpcut.to>
2018-04-29 16:33:28 +0000

    add in line to delete migration files.

--- deployment/aliases.sh
+++ deployment/aliases.sh
@@ -1,4 +1,4 @@
-alias _cd_deployment='cd /code/jumpjut/deployment'
+alias _cd_deployment='cd /code/jumpcut/deployment'
 alias start_containers='(_cd_deployment && docker-compose up -d)'
 alias stop_containers='(_cd_deployment && docker-compose down)'
 alias pull_containers='(_cd_deployment && docker-compose pull)'
@@ -9,6 +9,10 @@ function m() {
     (_cd_deployment && docker-compose run --rm www src/manage.py "$@")
 }
 
+function delete_migrations() {
+    (_cd_deployment && docker-compose run --rm www rm -f src/*/migrations/[0-9]*.py )
+}
+
 alias shell='m shell_plus'
-alias clean_slate='m reset_db --noinput && m migrate && m loaddata foundation'
+alias clean_slate='delete_migrations && m reset_db --noinput && m migrate && m loaddata foundation'
 alias fixtures='m loaddata dev'
