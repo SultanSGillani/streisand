5b2b8f264f632fd8e4a8af15fa538952aa29978e
Saltman <admin@example.com>
2018-05-01 13:32:40 -0400

    graphql 2.0 fix

--- backend/src/forums/schema.py
+++ backend/src/forums/schema.py
@@ -39,7 +39,7 @@ class Query(object):
     def resolve_all_groups(self, info, **kwargs):
         return ForumGroup.objects.all()
 
-    def resolve_group(self, args):
+    def resolve_group(self, args, info):
         id = args.get('id')
         if id is not None:
             return ForumGroup.objects.get(pk=id)
@@ -57,7 +57,7 @@ class Query(object):
     def resolve_all_threads(self, info, **kwargs):
         return ForumThread.objects.all()
 
-    def resolve_thread(self, args):
+    def resolve_thread(self, args, info)
         id = args.get('id')
         if id is not None:
             return ForumThread.objects.get(pk=id)
@@ -66,7 +66,7 @@ class Query(object):
     def resolve_all_posts(self, info, **kwargs):
         return ForumPost.objects.all()
 
-    def resolve_post(self, args):
+    def resolve_post(self, args, info)
         id = args.get('id')
         if id is not None:
             return ForumPost.objects.get(pk=id)
