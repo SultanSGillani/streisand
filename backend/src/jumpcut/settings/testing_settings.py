b67cb941496d0d6db1c069f2f2f6ad7b2b3c6d42
Saltman <admin@example.com>
2018-05-03 22:21:44 +0000

    Update testing_settings.py

--- backend/src/jumpcut/settings/testing_settings.py
+++ backend/src/jumpcut/settings/testing_settings.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python
+
+import os
+
 from decouple import config
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
 DEBUG = config('DEBUG', cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+INSTALLED_APPS = [
+
+    # Default apps
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'www.apps.SuitConfig',
+    'decouple',
+
+    # Local apps
+    'comments',
+    'films',
+    'forums',
+    'imdb',
+    'invites',
+    'media_formats',
+    'mediainfo',
+    'rotten_tomatoes',
+    'tests',
+    'torrent_requests',
+    'torrent_stats',
+    'torrents',
+    'tracker',
+    'users',
+    'wiki',
+    'www',
+
+    # Third party
+    'django_extensions',
+
+]
+
+
 WSGI_APPLICATION = 'jumpcut.testing_wsgi.application'
 
 SECRET_KEY = config('SECRET_KEY')
