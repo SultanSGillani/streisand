9cdb6e4ff887182c2a18f526baec6e9aa6602a48
saltman <saltman@jumpcut.to>
2018-05-15 19:53:53 -0400

    adjust frontend template url

--- backend/src/www/urls.py
+++ backend/src/www/urls.py
@@ -6,10 +6,14 @@ from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import re_path
 from django.views.generic import TemplateView
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     # API
     url(r'^api/v1/', include('api.urls')),
+
+    # API Core-Schema Docs TODO: Update this when better Api Docs come out and work.
+    url(r'^docs/', include_docs_urls(title='jumpcut API v1', public=False)),
 
     # Docs that need updating. Made with Sphinx
     url(r'^model-docs/', include('docs.urls')),
@@ -25,6 +29,7 @@ urlpatterns = [
 
     # Authentication
     url(r'^su/', include('django_su.urls')),
+    url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
 
 DEBUG = config('DEBUG', cast=bool)
@@ -36,6 +41,3 @@ if DEBUG:
         url(r'^dev/', staff_member_required(TemplateView.as_view(template_name='dev.html')))
     ] + urlpatterns
 
-# Anything else gets passed to the frontend
-
-urlpatterns.append(re_path('.*/', TemplateView.as_view(template_name='index.html')))
