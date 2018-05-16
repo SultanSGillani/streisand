59ae359b6d11841c775ddd0d7d676aec8f72c4f8
saltman <saltman@jumpcut.to>
2018-05-16 01:24:03 -0400

    linting

--- backend/src/api/wiki/views.py
+++ backend/src/api/wiki/views.py
@@ -1,16 +1,13 @@
 from django.db.models import Q
-
+from rest_framework import mixins
 from rest_framework.filters import (
     SearchFilter,
     OrderingFilter,
 )
-from rest_framework.response import Response
-
-from rest_framework import mixins
-from wiki.models import WikiArticle
-
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
+
+from wiki.models import WikiArticle
 from .serializers import WikiCreateUpdateDestroySerializer, WikiBodySerializer, WikiViewListOnlySerializer
 
 
