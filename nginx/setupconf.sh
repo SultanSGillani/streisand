83df7e15a89acac1b2b07e144f037a8d9cedc884
saltman <saltman@jumpcut.to>
2018-05-24 01:09:57 -0400

    more nginx deployment updates

--- nginx/setupconf.sh
+++ nginx/setupconf.sh
@@ -2,7 +2,7 @@
 cd /etc/nginx
 rm nginx.conf
 if ls ssl/*.pem &> /dev/null; then
-    mv nginx_ssl.conf nginx.conf
+    mv nginx-production.conf nginx.conf
     CRT=$(ls ssl/fullchain.pem | head -n 1)
     KEY=$(ls ssl/privkey.pem | head -n 1)
     echo "using ssl, privkey.pem=${KEY} fullchain.pem=${CRT}"
@@ -10,5 +10,5 @@ if ls ssl/*.pem &> /dev/null; then
     sed -i "s#ssl/privkey.pem#$KEY#g" /etc/nginx/nginx.conf
 else
     echo "not using ssl"
-    mv /etc/nginx/nginx_nossl.conf /etc/nginx/nginx.conf
+    mv /etc/nginx/nginx-develop.conf /etc/nginx/nginx.conf
 fi
