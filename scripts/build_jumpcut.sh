6d0c8f44288e321eb2bfaf5b8dae3d492cbba9e8
saltman <saltman@jumpcut.to>
2018-05-29 13:41:21 -0400

    deplyment

--- scripts/build_jumpcut.sh
+++ scripts/build_jumpcut.sh
@@ -3,5 +3,25 @@
 set -eu -o pipefail
 set -x
 
+IMAGE=${1:-jumpcut}
+
+
+# Ensure we are in the right (root) directory
+cd "${0%/*}"
+cd ..
+
+source scripts/env.sh
 dcprod build
+# Build frontend distribution
+docker build --pull -t frontend frontend --file frontend/Dockerfile-production
+
+# Build nginx distribution
+
+docker build --pull -t nginx nginx --file nginx/Dockerfile
+
+cd "${0%/*}"
+cd ..
+cd backend
+# Build production image
+docker build --pull -t ${IMAGE} -f Dockerfile-production .
 
