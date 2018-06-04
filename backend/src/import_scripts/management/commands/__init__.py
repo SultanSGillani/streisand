536ae7bb9f61a58f55b54705b5e649979cacb296
saltman <saltman@jumpcut.to>
2018-06-04 15:35:00 -0400

    import_scripts

--- backend/src/import_scripts/management/commands/__init__.py
+++ backend/src/import_scripts/management/commands/__init__.py
@@ -2,20 +2,21 @@
 
 import MySQLdb
 from MySQLdb.cursors import SSDictCursor
-from tqdm import tqdm
 from decouple import config
 from django.core.management.base import BaseCommand
+from tqdm import tqdm
 
 
 class MySQLCommand(BaseCommand):
-
     DB_CONFIG = {
-        'user': config('MYSQL_DB_USER', default=''),
-        'password': config('MYSQL_DB_PASSWORD', default=''),
-        'db': config('MYSQL_DB', default=''),
-        'host': config('MYSQL_HOST', default=''),
+        'db': config('MYSQLDB_NAME'),
+        'user': config('MYSQL_USER'),
+        'password': config('MYSQL_PWD'),
+        'host': config('MYSQL_HOST'),
+        'port': config('MYSQL_PORT', cast=int),
         'cursorclass': SSDictCursor,
         'charset': 'latin1',
+
     }
 
     SQL = ""
