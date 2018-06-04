088d8ec19830bd1630f9cf1693087422595532a1
saltman <saltman@jumpcut.to>
2018-06-04 14:45:23 +0200

    do not chown scripts folder

--- scripts/build_jumpcut.sh
+++ scripts/build_jumpcut.sh
@@ -10,9 +10,6 @@ cd ..
 # Reformat config files for nginx
 ./config/nginxfmt.py ./config/nginx.conf ./backend/config/jumpcut.conf ./frontend/config/frontend.conf ./config/uwsgi_params ./config/ssl/ssl_params
 
-# Make sure www-data is the owner of these files
-sudo chown www-data:www-data ./config/nginx.conf ./backend/config/jumpcut.conf ./frontend/config/frontend.conf ./config/uwsgi_params ./config/ssl/ssl_params
-
 # Copy all files in config for nginx bare metal
 sudo cp ./config/uwsgi_params /etc/nginx/
 sudo cp ./config/nginx.conf /etc/nginx/
