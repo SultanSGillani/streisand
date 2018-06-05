45ff761b6815fe721452412a68910b0909593222
unitPower <unitpowers@gmail.com>
2018-06-05 16:48:09 -0700

    Add dummy torrent uplod control

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 
+import globals from '../utilities/globals';
 import LatestNews from '../components/LatestNews';
+import { makeRequest } from '../utilities/Requestor';
 
 export default function HomePage() {
     return (
         <div>
             <LatestNews />
+            <input className="mt-3" type="file" accept=".torrent" onChange={(event) => {
+                if (event.target.files && event.target.files.length) {
+                    const file = event.target.files[0];
+                    makeRequest({
+                        url: `${globals.baseUrl}/torrent-upload/`,
+                        method: 'POST',
+                        headers: {
+                            'Authorization': 'Token ' + localStorage['jumpcut.token']
+                        },
+                        data: file
+                    });
+                }
+            }}/>
         </div>
     );
 }
