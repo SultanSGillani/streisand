99a851296713684ece3625bbebf4f2cf708e16f5
saltman <saltman@jumpcut.to>
2018-06-07 23:48:26 -0400

    linting

--- backend/src/api/users/serializers.py
+++ backend/src/api/users/serializers.py
@@ -46,12 +46,13 @@ class UserIPSerializer(AllowFieldLimitingMixin, serializers.ModelSerializer):
         return obj.ip_address
 
 
-class AdminUserProfileSerializer(AllowFieldLimitingMixin, serializers.ModelSerializer):
+class AdminUserProfileSerializer(AllowFieldLimitingMixin,
+                                 serializers.ModelSerializer):
     user_class_rank = serializers.PrimaryKeyRelatedField(
         source='user_class', read_only=True)
     ip_addresses = UserIPSerializer(many=True, read_only=True)
     user_class = serializers.StringRelatedField()
-    
+
     class Meta:
         model = User
         fields = (
@@ -90,7 +91,8 @@ class AdminUserProfileSerializer(AllowFieldLimitingMixin, serializers.ModelSeria
         )
 
 
-class CurrentUserSerializer(AllowFieldLimitingMixin, serializers.ModelSerializer):
+class CurrentUserSerializer(AllowFieldLimitingMixin,
+                            serializers.ModelSerializer):
     user_class = serializers.StringRelatedField()
 
     class Meta:
@@ -197,18 +199,14 @@ class NewUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+        user = User.objects.create_user(validated_data['username'],
+                                        validated_data['email'],
                                         validated_data['password'])
         return user
 
     class Meta:
         model = User
-        fields = (
-            'id',
-            'username',
-            'email',
-            'password'
-        )
+        fields = ('id', 'username', 'email', 'password')
 
 
 class LoginUserSerializer(serializers.Serializer):
@@ -220,4 +218,5 @@ class LoginUserSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
 
-        raise serializers.ValidationError("Unable to log in with provided credentials.")
+        raise serializers.ValidationError(
+            "Unable to log in with provided credentials.")
