3ba8c39e86b8a0fff3107bccf1dcd62004d5a7ff
saltman <saltman@jumpcut.to>
2018-06-21 14:46:49 -0400

    update static and mesia file root dirs

--- backend/src/jumpcut/settings/www_settings.py
+++ backend/src/jumpcut/settings/www_settings.py
@@ -215,8 +215,7 @@ TEMPLATES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -224,7 +223,7 @@ STATICFILES_FINDERS = (
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ITEMS_PER_PAGE = 50
 
