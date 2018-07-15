a3e55a12aba27407a9b12d2988f63366be31769d
saltman <saltman@jumpcut.to>
2018-07-15 00:58:54 -0400

    fix incorrect stashed files

---
+++ frontend/src/reducers/__tests__/auth_spec.tsx
@@ -0,0 +1,24 @@
+import auth, { initialState } from '../auth';
+
+describe('reducers/auth', () => {
+    it ('returns empty state on "RECEIVED_LOGOUT"', () => {
+        expect(auth(initialState, {type: 'RECEIVED_LOGOUT'})).toMatchSnapshot();
+    });
+
+    it('returns empty state on "FAILED_AUTHENTICATION"', () => {
+        expect(auth(initialState, { type: 'FAILED_AUTHENTICATION' })).toMatchSnapshot();
+    });
+
+    it('returns state with isAuthenticating: true', () => {
+        const props = { username: 'jumpcut', password: 'jumpcut' }
+        expect(auth(initialState, { type: 'REQUEST_AUTHENTICATION', props })).toMatchSnapshot();
+    });
+
+    it('returns state with isAuthenticating: true and token: "12345"', () => {
+        expect(auth(initialState, { type: 'RECEIVED_AUTHENTICATION', token: '12345' })).toMatchSnapshot()
+    });
+
+    it('returns state with isAuthenticating: true and token: "12345"', () => {
+        expect(auth(initialState, { type: 'RECEIVED_REGISTRATION', token: '12345' })).toMatchSnapshot()
+    });
+});
