d88d6409d4f5073b68a2da7a9e0b811ac7f1edad
saltman <saltman@jumpcut.to>
2018-07-17 13:36:48 -0400

    fix collections2

--- frontend/src/state/collection/sagas.ts
+++ frontend/src/state/collection/sagas.ts
@@ -2,16 +2,16 @@ import { all } from 'redux-saga/effects';
 
 import { collectionSaga } from './actions/CollectionAction';
 import { collectionsSaga } from './actions/CollectionsAction';
-import { updateCollectionSaga } from './actions/UpdateCollectionAction';
 import { createCollectionSaga } from './actions/CreateCollectionAction';
 import { deleteCollectionSaga } from './actions/DeleteCollectionAction';
+import { updateCollectionSaga } from './actions/UpdateCollectionAction';
 
-export default function* allCollectionSaga() {
+export function* allCollectionSaga() {
     yield all([
-        collectionSaga(),
-        collectionsSaga(),
-        updateCollectionSaga(),
         createCollectionSaga(),
-        deleteCollectionSaga()
+        collectionsSaga(),
+        deleteCollectionSaga(),
+        collectionSaga(),
+        updateCollectionSaga()
     ]);
 }
