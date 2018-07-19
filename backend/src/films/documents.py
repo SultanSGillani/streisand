6dffb6f31bc16c46995d1fffc9c5b448d391b58b
saltman <saltman@jumpcut.to>
2018-07-19 19:17:32 -0400

    fix bad linting

--- backend/src/films/documents.py
+++ backend/src/films/documents.py
@@ -8,51 +8,44 @@ from films.models import Film
 INDEX = Index(settings.ELASTICSEARCH_INDEX_NAMES[__name__])
 
 # See Elasticsearch Indices API reference for available settings
-INDEX.settings(
-	number_of_shards=1,
-	number_of_replicas=1
-)
+INDEX.settings(number_of_shards=1, number_of_replicas=1)
 
 html_strip = analyzer(
-	'html_strip',
-	tokenizer="standard",
-	filter=["standard", "lowercase", "stop", "snowball"],
-	char_filter=["html_strip"]
-)
+    'html_strip',
+    tokenizer="standard",
+    filter=["standard", "lowercase", "stop", "snowball"],
+    char_filter=["html_strip"])
 
 
 @INDEX.doc_type
 class FilmDocument(DocType):
-	"""Book Elasticsearch document."""
+    """Book Elasticsearch document."""
 
-	id = fields.IntegerField(attr='id')
+    id = fields.IntegerField(attr='id')
 
-	title = fields.StringField(
-		analyzer=html_strip,
-		fields={
-			'raw': fields.StringField(analyzer='keyword'),
-		}
-	)
+    title = fields.StringField(
+        analyzer=html_strip,
+        fields={
+            'raw': fields.StringField(analyzer='keyword'),
+        })
 
-	description = fields.StringField(
-		analyzer=html_strip,
-		fields={
-			'raw': fields.StringField(analyzer='keyword'),
-		}
-	)
+    description = fields.StringField(
+        analyzer=html_strip,
+        fields={
+            'raw': fields.StringField(analyzer='keyword'),
+        })
 
-	year = fields.DateField()
+    year = fields.DateField()
 
-	genre_tags = fields.StringField(
-		attr='genre_tags_indexing',
-		analyzer=html_strip,
-		fields={
-			'raw': fields.StringField(analyzer='keyword', multi=True),
-		},
-		multi=True
-	)
+    genre_tags = fields.StringField(
+        attr='genre_tags_indexing',
+        analyzer=html_strip,
+        fields={
+            'raw': fields.StringField(analyzer='keyword', multi=True),
+        },
+        multi=True)
 
-	class Meta(object):
-		"""Meta options."""
+    class Meta(object):
+        """Meta options."""
 
-		model = Film  # The model associate with this DocType
+        model = Film  # The model associate with this DocType
--- backend/src/search_indexes/films/viewsets.py
+++ backend/src/search_indexes/films/viewsets.py
@@ -1,24 +1,22 @@
 from django_elasticsearch_dsl_drf.constants import (
-	LOOKUP_FILTER_TERMS,
-	LOOKUP_FILTER_RANGE,
-	LOOKUP_FILTER_PREFIX,
-	LOOKUP_FILTER_WILDCARD,
-	LOOKUP_QUERY_IN,
-	LOOKUP_QUERY_EXCLUDE,
+    LOOKUP_FILTER_TERMS,
+    LOOKUP_FILTER_RANGE,
+    LOOKUP_FILTER_PREFIX,
+    LOOKUP_FILTER_WILDCARD,
+    LOOKUP_QUERY_IN,
+    LOOKUP_QUERY_EXCLUDE,
 )
 from django_elasticsearch_dsl_drf.filter_backends import (
-	FacetedSearchFilterBackend,
-)
+    FacetedSearchFilterBackend,)
 
 from elasticsearch_dsl import (
-	DateHistogramFacet,
-	RangeFacet,
-	TermsFacet,
+    DateHistogramFacet,
+    RangeFacet,
 )
 from django_elasticsearch_dsl_drf.filter_backends import (
-	FilteringFilterBackend,
-	OrderingFilterBackend,
-	SearchFilterBackend,
+    FilteringFilterBackend,
+    OrderingFilterBackend,
+    SearchFilterBackend,
 )
 from django_elasticsearch_dsl_drf.viewsets import BaseDocumentViewSet
 
@@ -28,81 +26,85 @@ from .serializers import FilmDocumentSerializer
 
 
 class FilmDocumentView(BaseDocumentViewSet):
-	"""The BookDocument view."""
+    """The BookDocument view."""
 
-	document = FilmDocument
-	serializer_class = FilmDocumentSerializer
-	lookup_field = 'id'
-	filter_backends = [
-		FilteringFilterBackend,
-		OrderingFilterBackend,
-		SearchFilterBackend,
-		FacetedSearchFilterBackend,
-	]
-	# Define search fields
-	search_fields = (
-		'title',
-		'description',
-		'year',
-		'genre_tags',
-
-	)
-	# Define filtering fields
-	filter_fields = {
-		'id': {
-			'field': '_id',
-			'lookups': [
-				LOOKUP_FILTER_RANGE,
-				LOOKUP_QUERY_IN,
-			],
-		},
-		'genre_tags': {
-			'field': 'genre_tags',
-			'lookups': [
-				LOOKUP_FILTER_TERMS,
-				LOOKUP_FILTER_PREFIX,
-				LOOKUP_FILTER_WILDCARD,
-				LOOKUP_QUERY_IN,
-				LOOKUP_QUERY_EXCLUDE,
-			],
-		},
-		'genre_tags.raw': {
-			'field': 'genre_tags.raw',
-			'lookups': [
-				LOOKUP_FILTER_TERMS,
-				LOOKUP_FILTER_PREFIX,
-				LOOKUP_FILTER_WILDCARD,
-				LOOKUP_QUERY_IN,
-				LOOKUP_QUERY_EXCLUDE,
-			],
-		},
-	}
-	faceted_search_fields = {
-		'year': {
-			'field': 'year',
-			'facet': DateHistogramFacet,
-			'options': {
-				'interval': 'year',
-			}
-		},
-		'pages_count': {
-			'field': 'pages',
-			'facet': RangeFacet,
-			'options': {
-				'ranges': [
-					("<10", (None, 10)),
-					("11-20", (11, 20)),
-					("20-50", (20, 50)),
-					(">50", (50, None)),
-				]
-			}
-		},
-	}
-	# Define ordering fields
-	ordering_fields = {
-		'id': 'id',
-		'title': 'title.raw',
-		'year': 'year',
-	}
-	# Specify default ordering
-	ordering = ('id', 'title',)
+    document = FilmDocument
+    serializer_class = FilmDocumentSerializer
+    lookup_field = 'id'
+    filter_backends = [
+        FilteringFilterBackend,
+        OrderingFilterBackend,
+        SearchFilterBackend,
+        FacetedSearchFilterBackend,
+    ]
+    # Define search fields
+    search_fields = (
+        'title',
+        'description',
+        'year',
+        'genre_tags',
+    )
+    # Define filtering fields
+    filter_fields = {
+        'id': {
+            'field': '_id',
+            'lookups': [
+                LOOKUP_FILTER_RANGE,
+                LOOKUP_QUERY_IN,
+            ],
+        },
+        'genre_tags': {
+            'field':
+            'genre_tags',
+            'lookups': [
+                LOOKUP_FILTER_TERMS,
+                LOOKUP_FILTER_PREFIX,
+                LOOKUP_FILTER_WILDCARD,
+                LOOKUP_QUERY_IN,
+                LOOKUP_QUERY_EXCLUDE,
+            ],
+        },
+        'genre_tags.raw': {
+            'field':
+            'genre_tags.raw',
+            'lookups': [
+                LOOKUP_FILTER_TERMS,
+                LOOKUP_FILTER_PREFIX,
+                LOOKUP_FILTER_WILDCARD,
+                LOOKUP_QUERY_IN,
+                LOOKUP_QUERY_EXCLUDE,
+            ],
+        },
+    }
+    faceted_search_fields = {
+        'year': {
+            'field': 'year',
+            'facet': DateHistogramFacet,
+            'options': {
+                'interval': 'year',
+            }
+        },
+        'pages_count': {
+            'field': 'pages',
+            'facet': RangeFacet,
+            'options': {
+                'ranges': [
+                    ("<10", (None, 10)),
+                    ("11-20", (11, 20)),
+                    ("20-50", (20, 50)),
+                    (">50", (50, None)),
+                ]
+            }
+        },
+    }
+    # Define ordering fields
+    ordering_fields = {
+        'id': 'id',
+        'title': 'title.raw',
+        'year': 'year',
+    }
+    # Specify default ordering
+    ordering = (
+        'id',
+        'title',
+    )
