0ef6dd237501bf4b0895981286af323c06c2e587
saltman <saltman@jumpcut.to>
2018-08-02 00:08:42 -0400

    include frontend bundle in static

--- backend/src/jumpcut/settings/www_settings.py
+++ backend/src/jumpcut/settings/www_settings.py
@@ -180,7 +180,10 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/code/frontend/',
+]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
