86fc975ba6290d1fd1704a42d3b470eb5c213ba7
saltman <saltman@jumpcut.to>
2018-08-02 00:17:02 -0400

    oops

--- backend/src/jumpcut/settings/www_settings.py
+++ backend/src/jumpcut/settings/www_settings.py
@@ -181,7 +181,6 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     '/code/frontend/',
 ]
 STATICFILES_FINDERS = (
