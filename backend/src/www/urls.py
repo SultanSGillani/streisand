800d9d9cebc7e783b5098f1537f05f1beb25e1cb
saltman <saltman@jumpcut.to>
2018-08-09 17:52:03 -0400

    revert styling

--- backend/src/www/urls.py
+++ backend/src/www/urls.py
@@ -2,30 +2,31 @@
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
+
 from torrents.views import TorrentDownloadView
 
+
 urlpatterns = [
-	# API
-	url(r'^api/v1/', include('api.urls')),
-	url(r'^docs/', include('docs.urls')),
+    # API
+    url(r'^api/v1/', include('api.urls')),
 
-	# torrent urls
-	url(
-		regex=r'^torrent-download/(?P<torrent_id>\d+)/(?P<user_id>\d+)/(?P<unique_download_key>[0-9a-f]{64})/$',
-		view=TorrentDownloadView.as_view(),
-		name='torrent_download',
-	),
+    # Sphinx Documentation
+    url(r'^docs/', include('docs.urls')),
 
-	# Authentication
-	url(r'^su/', include('django_su.urls')),
+    # torrent urls
+    url(
+        regex=r'^torrent-download/(?P<torrent_id>\d+)/(?P<user_id>\d+)/(?P<unique_download_key>[0-9a-f]{64})/$',
+        view=TorrentDownloadView.as_view(),
+        name='torrent_download',
+    ),
 
+    # Authentication
+    url(r'^su/', include('django_su.urls')),
 
 ]
 
 if settings.DEBUG:
-	import debug_toolbar
-
-	urlpatterns.append(
-		url(r'^__debug__/', include(debug_toolbar.urls))
-	)
+    import debug_toolbar
+    urlpatterns.append(
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    )
