3e872618b2e0fd7fdf0ada078bd89cbdea478db4
saltman <saltman@jumpcut.to>
2018-09-19 15:32:27 -0400

    fix some scripts

--- scripts/build_jumpcut.sh
+++ scripts/build_jumpcut.sh
@@ -7,22 +7,19 @@ set -x
 cd "${0%/*}"
 cd ..
 
-# Reformat config files for nginx
-./frontend/nginx/nginxfmt.py ./frontend/nginx/nginx.conf ./frontend/nginx/conf.d/jumpcut.conf ./frontend/nginx/conf.d/tracker.conf ./frontend/nginx/conf.d/frontend.conf ./frontend/nginx/uwsgi_params ./frontend/nginx/ssl/ssl_params
-
 # Copy ssl certs
 sudo cp /etc/letsencrypt/live/pinigseu.xyz/fullchain.pem /code/jumpcut/frontend/nginx/ssl/fullchain.pem
 sudo cp /etc/letsencrypt/live/pinigseu.xyz/privkey.pem /code/jumpcut/frontend/nginx/ssl/privkey.pem
 sudo cp /etc/nginx/ssl/dhparam.pem /code/jumpcut/frontend/nginx/ssl/dhparam.pem
 
 # Build production
-sudo docker-compose -f docker-compose.production.yml -f docker-compose.production.yml build
+sudo docker-compose -f production.yml -f production.yml build
 
 # Build Docs
-sudo docker-compose -f docker-compose.production.yml run --rm api sphinx-build -b html /code/docs /code/docs/_build/html
+sudo docker-compose -f production.yml run --rm api sphinx-build -b html /code/docs /code/docs/_build/html
 
 # Start Production
-sudo docker-compose -f docker-compose.production.yml up -d --force-recreate
+sudo docker-compose -f production.yml up -d --force-recreate
 
 # Migrations
-./prod.sh m migrate
+sudo docker-compose -f production.yml run --rm api m migrate
--- scripts/create-review.sh
+++ scripts/create-review.sh
@@ -6,14 +6,12 @@ set -o errexit
 echo "Creating Review App"
 
 cd /code/$CI_ENVIRONMENT_SLUG
-cp ./frontend/.env.review.template ./frontend/.env
 cp ./backend/.env.review.template ./backend/.env
 sudo cp -R /home/gitlab-runner/ssl/* /code/$CI_ENVIRONMENT_SLUG/frontend/nginx/ssl/
 sudo cp -R /home/gitlab-runner/conf.d/* /code/$CI_ENVIRONMENT_SLUG/frontend/nginx/conf.d/
-docker-compose -f docker-compose.production.yml build
-docker-compose -f docker-compose.production.yml up -d --force-recreate
-docker-compose -f docker-compose.production.yml run --rm api invoke setup-db
-docker-compose -f docker-compose.production.yml run --rm api python src/manage.py collectstatic --noinput
-docker-compose -f docker-compose.production.yml run --rm api python src/manage.py loaddata films wiki
+docker-compose -f production.yml build
+docker-compose -f production.yml up -d --force-recreate
+docker-compose -f production.yml run --rm api invoke setup-db
+docker-compose -f production.yml run --rm api python src/manage.py loaddata films wiki
 
 echo "Done"
